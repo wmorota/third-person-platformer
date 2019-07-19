e5a8aed50a221a5d161e512d78d1109793309814
Wesley Morota <48344656+wmorota@users.noreply.github.com>
2019-07-19 12:52:25 -0400

    Added Global Score script
    - This script increments the user's score by gem's value when user collides with one
    - Green gems are worth 1000 and Yellow gems are worth 5000
    - We keep track of internalScore and currentScore (master score)

---
+++ Assets/Scripts/GlobalScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    public GameObject scoreBox;
+    public static int currentScore;  // MASTER SCORE
+    public int internalScore;       // viewing purposes
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalScore = currentScore;
+        scoreBox.GetComponent<Text>().text = "" + internalScore; 
+
+    }
+}
