64a2125e64f041970faa175e27fdbd2f5d82e0fd
Wesley Morota <48344656+wmorota@users.noreply.github.com>
2019-07-19 12:50:43 -0400

    Added GlobalTimer script
    - This script decrements the timer by 1 per second that has passed within the game

---
+++ Assets/Scripts/GlobalTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GlobalTimer : MonoBehaviour
+{
+
+  public GameObject timeDisplay01;
+  public GameObject timeDisplay02;
+  public bool decrementTime = false;
+  public int seconds = 151;
+
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(decrementTime  == false)
+      {
+        StartCoroutine(SubtractSecond());
+      }
+    }
+
+    IEnumerator SubtractSecond()
+    {
+      decrementTime = true;
+      seconds -= 1;
+      timeDisplay01.GetComponent<Text>().text = "" + seconds;
+      timeDisplay02.GetComponent<Text>().text = "" + seconds;
+      yield return new WaitForSeconds(1) ;
+      decrementTime = false;
+    }
+
+
+
+}
