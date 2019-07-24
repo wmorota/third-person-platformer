49ad885c3f15b176dd7520b823d0997d5647f54b
Wesley Morota <48344656+wmorota@users.noreply.github.com>
2019-07-24 17:17:59 -0400

    Added and Updated scripts for falling off level platform

--- Assets/Scripts/LevelDeath.cs
+++ Assets/Scripts/LevelDeath.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelDeath : MonoBehaviour
 {
+
+  public GameObject youFallen;
+  public GameObject levelAudio;
+  public GameObject fadeOutAnimation;
+
     void OnTriggerEnter()
     {
-      SceneManager.LoadScene(0) ; 
+      StartCoroutine(YouFellOff());
     }
+
+    IEnumerator YouFellOff ()
+    {
+      youFallen.SetActive(true);
+      levelAudio.SetActive(false);
+      yield return new WaitForSeconds(2);
+      fadeOutAnimation.SetActive(true);
+      yield return new WaitForSeconds(1);
+      SceneManager.LoadScene(0) ;
+    }
+
 }
