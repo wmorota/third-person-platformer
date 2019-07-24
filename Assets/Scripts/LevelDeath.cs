f60bf09c46a5d88628c1533407daf6837f210ddb
Wesley Morota <48344656+wmorota@users.noreply.github.com>
2019-07-24 15:24:13 -0400

    Added functionality that redirects player to the beginning if user falls off level stage
    - user goes back to beginning
    - timer resets
    - HOWEVER score has not reset yet

---
+++ Assets/Scripts/LevelDeath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDeath : MonoBehaviour
+{
+    void OnTriggerEnter()
+    {
+      SceneManager.LoadScene(0) ; 
+    }
+}
