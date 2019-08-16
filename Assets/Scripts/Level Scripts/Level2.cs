282fe4159d2fadc1526fe833df089952d9916f83
Wesley Morota <48344656+wmorota@users.noreply.github.com>
2019-08-16 16:37:45 -0400

    Updated Level 2 script

---
+++ Assets/Scripts/Level Scripts/Level2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2 : MonoBehaviour
+{
+  public GameObject fadeIn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      RedirectToLevel.redirectToLevel = 4;
+      RedirectToLevel.nextLevel = 2;  // for now since no level 3 
+      StartCoroutine(FadeInOff());
+    }
+
+    IEnumerator FadeInOff()
+    {
+      yield return new WaitForSeconds(1);
+      fadeIn.SetActive(false);
+    }
+
+
+}
